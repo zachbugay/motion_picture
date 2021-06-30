1aff333290e6b7950f67644ea0ba058b831974a5
Zach Bugay <zbugay@gmail.com>
2021-06-30 15:15:22 -0400

    Initial commit of client.

---
+++ services/client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <motion-pictures msg="Hello Dude!"></motion-pictures>
+  </div>
+</template>
+
+<script>
+import MotionPictures from './components/MotionPictures.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MotionPictures
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ services/client/src/components/MotionPictures.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Motion Pictures</h1>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "MotionPictures",
+    props: {
+      msg: String
+    }
+  }
+</script>
